3378706b19a4760f4a7638d0cabca6c7d2562081
Baba Jaguska <minja.n.belic@gmail.com>
2016-02-07 16:32:37 +0100

    Create TitleCase.js

---
+++ TitleCase.js
@@ -0,0 +1,15 @@
+// I guess the point is here that there are functions str.toUpperCase() and str.toLowerCase()
+// and that they have to have these brackets!
+
+function titleCase(str) {
+  var words=str.split(" ");
+  words=words.map(function(x){var temp=x.split("");
+                        temp[0]=temp[0].toUpperCase();
+                              for (i=1;i<temp.length;i++){
+                                temp[i]=temp[i].toLowerCase();
+                              }
+                        return temp.join('');});
+  return words.join(" ");
+}
+
+titleCase("I'm a little tea pot");
